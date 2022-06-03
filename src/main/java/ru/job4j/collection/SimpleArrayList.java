ef94b5358da6f91c57d038f93feaa3d2d240131d
Andrey Morozov <selove.jara@gmail.com>
2022-06-03 12:57:50 +0400

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,78 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (container.length == size) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        T temp = get(index);
+        container[index] = newValue;
+        return temp;
+    }
+
+    @Override
+    public T remove(int index) {
+        T temp = get(index);
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount--;
+        return temp;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int expectedModCount = modCount;
+            int pointer = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+
+                    throw new ConcurrentModificationException();
+                }
+                return pointer < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[pointer++];
+            }
+
+        };
+    }
+}
