6fc61d99a6ff192eb13e17759037f5463d9026c5
Andrey Morozov <selove.jara@gmail.com>
2022-06-23 10:43:00 +0400

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String element : lines) {
+                int number = Integer.parseInt(element);
+                if (number % 2 == 0) {
+                    System.out.println("Четное число: " + number);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
