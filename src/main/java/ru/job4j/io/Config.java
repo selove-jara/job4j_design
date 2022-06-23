3a6f9dbaeaabd098bbf00474f329e438cab37421
Andrey Morozov <selove.jara@gmail.com>
2022-06-23 21:20:48 +0400

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,56 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            String line;
+            while ((line = read.readLine()) != null) {
+                String[] division = line.split("=", 2);
+                if (!line.isEmpty() && !line.startsWith("#")) {
+                    if (division.length == 2 && !division[0].isEmpty() && !division[1].isEmpty()) {
+                        values.put(division[0], division[1]);
+                    } else {
+                        throw new IllegalArgumentException("Broken pair (key = value)");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/comments.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+    }
+
+    @Test
+    public void whenPairNull() {
+        String path = "./data/empty.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("#hibernate.connection.username"), is(Matchers.nullValue()));
+    }
+
+    @Test
+    public void whenMultiplePairDivider() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.driver_class"), is("org=postgresql=Driver"));
+    }
+
+    @Test
+    public void whenPairWithException() {
+        thrown.expect(IllegalArgumentException.class);
+        String path = "./data/exception.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
