24db53cf08e962bd1542dae43e413dc6efce603e
Andrey Morozov <selove.jara@gmail.com>
2022-06-24 19:32:27 +0400

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("C:\\projects\\job4j_design\\src\\main\\java\\ru\\job4j\\io");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.length());
+            System.out.println(subfile.getName());
+        }
+    }
+}
