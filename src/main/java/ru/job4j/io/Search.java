928be6c0a7248f1e5b89f6917578aaced55e29c2
Andrey Morozov <selove.jara@gmail.com>
2022-06-26 10:49:58 +0400

    5. Валидация параметров запуска. [#246865]

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        File path = new File(args[0]);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        if (path.isAbsolute() && !path.isDirectory()) {
+            throw new IllegalArgumentException("Directory is missing");
+        }
+
+    }
+}
