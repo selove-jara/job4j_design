cf3cc122b28c224f8f5a4ce95c3c3e41c53f08aa
selov <selove.jara@gmail.com>
2024-04-22 10:31:13 +0400

    2. Создать контейнер на базе связанного списка  [#159 #308710]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,75 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> head;
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> node = head;
+        for (int count = 0; count < index; count++) {
+            node = node.next;
+        }
+        return node.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        int expectedModCount = modCount;
+        return new Iterator<E>() {
+            Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> node = current;
+                current = current.next;
+                return node.item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
