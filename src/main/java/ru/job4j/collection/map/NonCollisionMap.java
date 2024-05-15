b721618cfae2f06202df08a46192ae4268de9609
selov <selove.jara@gmail.com>
2024-05-15 09:07:09 +0400

    10. Реализовать собственную структуру данных - HashMap [#1008 #312347]

---
+++ src/main/java/ru/job4j/collection/map/NonCollisionMap.java
@@ -0,0 +1,120 @@
+package ru.job4j.collection.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
+
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private int capacity = 8;
+
+    private int count = 0;
+
+    private int modCount = 0;
+
+    private MapEntry<K, V>[] table = new MapEntry[capacity];
+
+    @Override
+    public boolean put(K key, V value) {
+        if (count >= capacity * LOAD_FACTOR) {
+            expand();
+        }
+
+        boolean result = false;
+        int hash = hash(Objects.hashCode(key));
+        int index = indexFor(hash);
+        if (table[index] == null) {
+            table[index] = new MapEntry<>(key, value);
+            modCount++;
+            count++;
+            result = true;
+        }
+        return result;
+    }
+
+    private int hash(int hashCode) {
+        return hashCode == 0 ? 0 : hashCode ^ (hashCode >>> 16);
+    }
+
+    private int indexFor(int hash) {
+        return hash & (capacity - 1);
+    }
+
+    private void expand() {
+        capacity *= 2;
+        MapEntry<K, V>[] newTable = new MapEntry[capacity];
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                newTable[indexFor(hash(Objects.hashCode(table[i].key)))] = table[i];
+            }
+        }
+        table = newTable;
+    }
+
+    @Override
+    public V get(K key) {
+        V result = null;
+        int index = indexFor(hash(Objects.hashCode(key)));
+        if (table[index] != null
+                && Objects.hashCode(table[index].key) == Objects.hashCode(key)
+                && Objects.equals(table[index].key, key)) {
+            result = table[index].value;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        boolean result = false;
+        int index = indexFor(hash(Objects.hashCode(key)));
+        if (get(key) != null) {
+            table[index] = null;
+            result = true;
+            count--;
+            modCount++;
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return new Iterator<>() {
+            private int index;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                while (index < table.length && table[index] == null) {
+
+                    index++;
+                }
+                return index < table.length;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return table[index++].key;
+            }
+        };
+    }
+
+    private static class MapEntry<K, V> {
+
+        K key;
+        V value;
+
+        public MapEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
