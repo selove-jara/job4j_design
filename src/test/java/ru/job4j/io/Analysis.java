e343c9f053f5e3e5612c5cf0a516ca5214d42523
selov <selove.jara@gmail.com>
2024-05-20 21:12:06 +0400

    7. Анализ доступности сервера [#859 #315900]

---
+++ src/test/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    private boolean status = true;
+
+    public void unavailable(String source, String target) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter output = new PrintWriter(
+                     new BufferedOutputStream(
+                             new FileOutputStream(target)
+                     ))) {
+            reader.lines()
+                    .forEach(s -> {
+                        String[] strings = s.split(" ");
+                        if (("500".equals(strings[0]) || "400".equals(strings[0])) && status) {
+                            status = false;
+                            output.append(strings[1]).append(";");
+                        } else if (("200".equals(strings[0]) || "300".equals(strings[0])) && !status) {
+                            status = true;
+                            output.append(strings[1]).append(";").append(System.lineSeparator());
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
